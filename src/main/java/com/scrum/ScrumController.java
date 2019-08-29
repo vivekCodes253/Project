84eb89130b37d902042792d433f86bd64261dfad
=V <=google.com>
2019-08-29 09:37:46 +0000

    addedtaskhandler

--- src/main/java/com/scrum/ScrumController.java
+++ src/main/java/com/scrum/ScrumController.java
@@ -1,8 +1,11 @@
 package com.scrum;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.scrum.business.TaskRepository;
 import com.scrum.log.LoggerMain;
 
 
@@ -10,12 +13,19 @@ import com.scrum.log.LoggerMain;
 public class ScrumController {
 	LoggerMain lm = new LoggerMain();
 	
+	@Autowired
+	TaskRepository taskRepo;
+	
 	@RequestMapping("/")
 	public String hello() {	
 		LoggerMain.logger.warn("New Introduction");
 		return "Landing";
 	}
 	
-	
+	@RequestMapping("/tasks")
+	String taskHandler(Model model) {
+		model.addAttribute("tasks", taskRepo.allTasks());
+		return "tasks";
+	}
 	
 }
